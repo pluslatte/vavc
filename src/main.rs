5dccc1650a23ec3cf8931589b05ffea592be3629
pluslatte <debrisfield0201@gmail.com>
2025-09-02 22:37:12 +0900

    fetch してログに吐く

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,8 @@ mod secret;
 
 use clap::{Parser, Subcommand, command};
 use std::io::{self, Write};
+use std::time::Duration;
+use tokio::time::sleep;
 use vrchatapi::apis;
 use vrchatapi::apis::configuration::Configuration;
 
@@ -91,31 +93,58 @@ async fn handler_auth(username: Option<String>, password: Option<String>, check:
 
 async fn handler_fetch(config: Configuration) {
     println!("Fetching avatars...");
-    let avatars = apis::avatars_api::search_avatars(
-        &config,
-        Some(false),
-        Some(vrchatapi::models::SortOption::Name),
-        Some("me"),
-        None,
-        Some(60),
-        None,
-        None,
-        None,
-        None,
-        Some(vrchatapi::models::ReleaseStatus::All),
-        None,
-        None,
-        None,
-    )
-    .await;
 
-    if let Ok(avatars) = avatars {
-        avatars.iter().for_each(|avatar| {
-            println!("{}: {}", avatar.name, avatar.id);
-        });
-    } else {
-        eprintln!("Failed to fetch avatars: {}", avatars.err().unwrap());
+    let mut avatar_count: usize = 0;
+
+    loop {
+        let avatars = apis::avatars_api::search_avatars(
+            &config,
+            Some(false),
+            Some(vrchatapi::models::SortOption::Name),
+            Some("me"),
+            None,
+            Some(60),
+            None,
+            Some(avatar_count.try_into().expect("Negative avatar count wtf")),
+            None,
+            None,
+            Some(vrchatapi::models::ReleaseStatus::All),
+            None,
+            None,
+            None,
+        )
+        .await;
+
+        if let Ok(avatars) = avatars {
+            let got = avatars.len();
+            if got == 0 {
+                break;
+            }
+
+            println!(
+                "Fetched {} avatars, total so far: {}",
+                got,
+                avatar_count + got
+            );
+
+            avatar_count += got;
+            avatars.iter().for_each(|avatar| {
+                println!("{}: {}", avatar.name, avatar.id);
+            });
+
+            println!("Sleep for 5 seconds to avoid rate limiting...");
+            sleep(Duration::from_secs(5)).await; // To avoid rate limiting
+            continue;
+        } else {
+            eprintln!("Failed to fetch avatars: {}", avatars.err().unwrap());
+            break;
+        }
     }
+
+    println!(
+        "Finished fetching avatars. Total avatars fetched: {}",
+        avatar_count
+    );
 }
 
 async fn handler_search(config: Configuration, query: String) {}
